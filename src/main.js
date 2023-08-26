5f8add099bd706338c8c7b9b885a0e1593819913
Zaariy <mohamedzaariy@gmail.com>
2023-08-26 12:59:47 +0100

    update javascript code

--- src/main.js
+++ src/main.js
@@ -1,120 +1,124 @@
-// data
-let data = {
+/*
+
+  /===>  Steps  <===/
+
+  1 : Get all inputs that we have. and put them into variables
+  2 : Collect all inputs data 
+  3 : Check if  there any  Errors
+  4 : Calculate data 
+  5 : Add results on screen  / [DOM]
+  6 : Run main() function 
+  7 : We are finshed
+
+*/
+
+// Data input
+let inputData = {
   bill: 0,
   tip: 0,
   people: 0,
   tipHtmlElement: 0,
 };
-// public varibles
+
+// Data output
+let outputData = {
+  tip: 0,
+  total: 0,
+};
+
+//  variables
 const bill = document.getElementById("bill");
 const people = document.getElementById("numberPeople");
 const customTip = document.getElementById("customTip");
 const resetButton = document.getElementById("reset");
 const errorMessage = document.getElementById("error-message");
-//
+const tipsPercentage = document.getElementById("tipsPercentage");
 
-function checkValue(value) {
-  if (Number(value) !== 0) {
-    return true;
+// output varibles
+const total = document.getElementById("total");
+const tip = document.getElementById("tip");
+
+//First checkValueFunction(data) => check if the value !== 0 and not equal a NaN
+function checkValueFunction(value) {
+  if (isNaN(value)) {
+    return false;
   }
-  return false;
-}
-
-function getValueTip(tip, element) {
-  if (checkValue(tip)) {
-    data.tip = Number(tip);
-    data.tipHtmlElement = element;
-    element.classList.add("selected-tip");
-    caluclateTip();
+  if (value === 0) {
+    return false;
   }
+  if (value.length == 0) {
+    return false;
+  }
+
+  return true;
 }
 
-function CollectData() {
-  // bill
-  bill.addEventListener("mouseout", (e) => {
-    if (checkValue(e.target.value)) {
-      data.bill = Number(e.target.value);
-      changeStateOfResetBotton();
-      caluclateTip();
+// CollectDataFunction () => data Ready To use
+function CollectDataFunction() {
+  // take bill input
+  if (checkValueFunction(bill.value)) {
+    inputData.bill = Number(bill.value);
+  }
+
+  //take tip data
+  tipsPercentage.addEventListener("click", (element) => {
+    var elementSelected = element.target.value;
+    if (checkValueFunction(elementSelected)) {
+      inputData.tip = elementSelected;
+      // add style to the selected tip
+      element.target.classList.add("selected-tip");
+
+      // store the element
+      inputData.tipHtmlElement = element.target;
     }
   });
 
-  bill.addEventListener("focusout", (e) => {
-    if (checkValue(e.target.value)) {
-      data.bill = Number(e.target.value);
+  // take number of people
+  if (checkValueFunction(people.value)) {
+    inputData.people = Number(people.value);
+  }
 
-      caluclateTip();
-      changeStateOfResetBotton();
-    }
-  });
-
-  // customTip
-  customTip.addEventListener("mouseout", (e) => {
-    if (checkValue(e.target.value)) {
-      data.tip = Number(e.target.value);
-      caluclateTip();
-      changeStateOfResetBotton();
-    }
-  });
-  customTip.addEventListener("focusout", (e) => {
-    if (checkValue(e.target.value)) {
-      data.tip = Number(e.target.value);
-      changeStateOfResetBotton();
-
-      caluclateTip();
-    }
-  });
-
-  // people
-  people.addEventListener("mouseout", (e) => {
-    if (checkValue(e.target.value)) {
-      people.classList.remove("error-border");
-      errorMessage.style.display = "none";
-      data.people = Number(e.target.value);
-      changeStateOfResetBotton();
-      caluclateTip();
-    } else {
-      people.classList.add("error-border");
-      errorMessage.style.display = "block";
-    }
-  });
-
-  people.addEventListener("focusout", (e) => {
-    if (checkValue(e.target.value)) {
-      data.people = Number(e.target.value);
-      changeStateOfResetBotton();
-    }
-    caluclateTip();
-  });
-}
-
-function caluclateTip() {
-  if (data.bill !== 0 && data.people !== 0 && data.tip !== 0) {
-    const tip = (data.tip * data.bill) / (100 * data.people);
-    const total = data.bill / data.people + tip;
-
-    // set Data on DOM
-    setDataOnDOM(tip.toFixed(2), total.toFixed(2));
+  // take custom tip
+  if (checkValueFunction(customTip.value)) {
+    inputData.tip = Number(customTip.value);
   }
 }
+// calc result
+function caluclateTip(bill, tip, people) {
+  if (bill !== 0 && people !== 0 && tip !== 0) {
+    const tipOutput = (tip * bill) / (100 * people);
+    const total = bill / people + tipOutput;
 
-function setDataOnDOM(tip, total) {
-  document.getElementById("tip").textContent = `$${tip}`;
-  document.getElementById("total").textContent = `$${total}`;
+    outputData.tip = tipOutput.toFixed(2);
+    outputData.total = total.toFixed(2);
+  }
+}
+// push result of caluclation in DOM
+function addResultToDOM() {
+  tip.textContent = `$${outputData.tip}`;
+  total.textContent = `$${outputData.total}`;
+}
+
+function checkErrorsFunction() {
+  // inputData people
+  if (checkValueFunction(inputData.people) === false) {
+    errorMessage.style.display = "block";
+  }
 }
 
 function reset() {
-  if (data.bill !== 0 || data.people !== 0 || data.tip !== 0) {
+  if (inputData.bill !== 0 || inputData.people !== 0 || inputData.tip !== 0) {
     // Reset data
-    data = { ...data, tip: 0, bill: 0, people: 0 };
+    inputData = { ...inputData, tip: 0, bill: 0, people: 0 };
     //Reset
-    setDataOnDOM(0, 0);
+    total.textContent = "$0";
+    tip.textContent = "$0";
     //Reset inputs {bill , tip , people}
-    document.getElementById("bill").value = "0";
-    document.getElementById("numberPeople").value = "0";
-    document.getElementById("customTip").value = "0";
+    bill.value = "";
+    people.value = "";
+    customTip.value = "";
     // reset tip
-    data.tipHtmlElement.classList.remove("selected-tip");
+    inputData.tipHtmlElement.classList.remove("selected-tip");
     // Reset button
     resetButton.style.cursor = "not-allowed";
     resetButton.classList.remove("active-reset-button");
@@ -125,6 +129,25 @@ function changeStateOfResetBotton() {
   resetButton.classList.add("active-reset-button");
   resetButton.style.cursor = "pointer";
 }
-CollectData();
 
-document.getElementById("reset").addEventListener("click", reset);
+function main() {
+  CollectDataFunction();
+  // callc data
+  people.addEventListener("change", () => {
+    //collect all Data
+    CollectDataFunction();
+    // check if any Errors
+    checkErrorsFunction();
+    // calculate data
+    caluclateTip(inputData.bill, inputData.tip, inputData.people);
+    // add data on DOM
+    addResultToDOM();
+    // button
+    changeStateOfResetBotton();
+  });
+
+  document.getElementById("reset").addEventListener("click", reset);
+}
+
+// Run
+main();
